4b6cc7500e09e56e6c3565d2c0c32ff7034b412a
hemankpatwal <98538477+hemankpatwal@users.noreply.github.com>
2022-02-19 13:36:20 +0530

    first

---
+++ src/components/header/Header.tsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+const Header = () => (
+    <div className="w-full">
+       <div className="flex w-full text-white">
+           <div>
+               Hemank
+            </div>
+           <div>
+                <div>
+                    About
+                </div>
+                <div>
+                    Services
+                </div>
+                <div>
+                    Projects
+                </div>
+           </div>
+        </div> 
+    </div>
+)
+
+export default Header
