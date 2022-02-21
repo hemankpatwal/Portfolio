eae8e1c86e72a0130e7fa75d837a557922b08c5f
hemankpatwal <98538477+hemankpatwal@users.noreply.github.com>
2022-02-21 23:28:39 +0530

    4

--- frontend/src/components/landing/Landing.tsx
+++ frontend/src/components/landing/Landing.tsx
@@ -4,12 +4,45 @@ const Landing = () => (
     <div className="w-full h-full">
            <div className="flex w-full "> 
              
+
+
+
+
                  <div className="w-full ml-[12%] pt-[15%]">
 
                            <div className="font-Paprika text-8xl  ">
                                   Hi I'm Hemank Patwal
                            </div>
-                  
+
+
+                           <div className="pt-[6%] h-[6.5rem]"> 
+                           <div className="bg-slate-200 rounded-xl  w-[86%]"> 
+                                  <div className=" font- Quattrocento text-2xl flex justify-center items-center  ">
+                                         --  Hi! I’m a Web Developer. I develop creative, responsive and user friendly websites for clients all over the globe.
+                                   </div>
+                          </div>       
+                          </div>
+
+                          <div className="flex pt-[10%] font-Montserrat text-2xl font-semibold">
+                               <div className="bg-pink-200 border rounded-full w-[25%] flex justify-center items-center h-[3.5rem]">
+                                     <span>
+                                         Let's Talk
+                                      </span>   
+                                      <span className="ml-[10%]">
+                                          <img src="/images/icon1.png" /> 
+                                      </span>
+                               </div>
+
+                               <div className="flex justify-center items-center border rounded-full h-[3.5rem] border-pink-300 ml-[10%] w-[25%]">
+                                   Portfolio
+                               </div>
+                          </div>
+
+                          <div className="pt-[20%]">
+                               <img src="/images/arrow1.png" />
+                          </div>
+
+
                  </div>
 
 
