e486ecef8a8f2057b256f032bec677fa4b17eb4d
hemankpatwal <98538477+hemankpatwal@users.noreply.github.com>
2022-02-23 00:30:28 +0530

    Update Landing.tsx

--- src/components/landing/Landing.tsx
+++ src/components/landing/Landing.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Landing = () => (
           <div className="w-full h-full">
 
-                                     <div className="flex w-full "> 
+                                   <div className="flex w-full "> 
              
 
 
@@ -20,75 +20,74 @@ const Landing = () => (
 
                                                                                                               <div className=" font- Quattrocento text-2xl flex justify-center items-center  ">
                                                                                                                         --  Hi! I’m a Web Developer. I develop creative, responsive and user friendly websites for clients all over the globe.
-                                   </div>
-                          </div>       
-                          </div>
+                                                                                                              </div>
+                                                                                          </div>       
+                                                                               </div>
 
-                          <div className="flex pt-[10%] font-Montserrat text-2xl font-semibold">
-                               <div className="bg-pink-200 border rounded-full w-[25%] flex justify-center items-center h-[3.5rem]">
-                                     <span>
-                                         Let's Talk
-                                      </span>   
-                                      <span className="ml-[10%]">
-                                          <img src="/images/icon1.png" /> 
-                                      </span>
-                               </div>
+                                                                               <div className="flex pt-[10%] font-Montserrat text-2xl font-semibold">
+                                                                                          <div className="bg-pink-200 border rounded-full w-[25%] flex justify-center items-center h-[3.5rem]">
+                                                                                                              <span>
+                                                                                                                         Let's Talk
+                                                                                                              </span>   
+                                                                                                              <span className="ml-[10%]">
+                                                                                                                       <img src="/images/icon1.png" /> 
+                                                                                                              </span>
+                                                                                         </div>
 
-                               <div className="flex justify-center items-center border rounded-full h-[3.5rem] border-pink-300 ml-[10%] w-[25%]">
-                                   Portfolio
-                               </div>
-                          </div>
+                                                                                        <div className="flex justify-center items-center border rounded-full h-[3.5rem] border-pink-300 ml-[10%] w-[25%]">
+                                                                                                            Portfolio
+                                                                                        </div>
+                                                                             </div>
                            
                          
 
-                          <div className="pt-[22%]">
-                               <img src="/images/arrow1.png" />
-                          </div>
+                                                                            <div className="pt-[22%]">
+                                                                                   <img src="/images/arrow1.png" />
+                                                                           </div>
                           
                          
-                 </div>
+                                                                      </div>
 
 
 
 
 
 
-                <div className=" w-full flex flex-col 
-                place-items-end">
+                                                                  <div className=" w-full flex flex-col place-items-end">
 
-                            <div className="ml-[0%]">
-                                 <img src="/images/img1.png" />
-                            </div>
+                                                                       <div className="ml-[0%]">
+                                                                                 <img src="/images/img1.png" />
+                                                                      </div>
 
-                            <div className="absolute pt-[10%]">
-                                 <img src="/images/img2.png" />
-                            </div>
+                                                                      <div className="absolute pt-[10%]">
+                                                                                 <img src="/images/img2.png" />
+                                                                      </div>
 
-                            <div className="z-10 absolute mr-[15%] pt-[10%] ">
-                                 <img src='/images/img3.png' />
-                            </div>
+                                                                      <div className="z-10 absolute mr-[15%] pt-[10%] ">
+                                                                                <img src='/images/img3.png' />
+                                                                      </div>
 
-                            <div className="space-y-5 mt-[40%] mr-[20%]">
-                               <div className=" text-left font-Salsa font-regular text-2xl">
-                                    Check Out
-                               </div>
+                                                                 <div className="space-y-5 mt-[40%] mr-[20%]">
+                                                                      <div className=" text-left font-Salsa font-regular text-2xl">
+                                                                                Check Out
+                                                                      </div>
 
-                               <div className="flex space-x-9">
-                                     <div>
-                                           <img src="/images/icon5.png" /> 
-                                     </div>
+                                                                      <div className="flex space-x-9">
+                                                                                <div>
+                                                                                          <img src="/images/icon5.png" /> 
+                                                                                </div>
                                            
-                                     <div>
-                                           <img src="/images/icon6.png" />
-                                     </div>
+                                                                                <div>
+                                                                                          <img src="/images/icon6.png" />
+                                                                                </div>
                                          
-                                     <div>
-                                           <img src="/images/icon7.png" />
-                                     </div>
+                                                                                <div>
+                                                                                          <img src="/images/icon7.png" />
+                                                                                </div>
                               
-                               </div>
-                          </div>
-             </div>
+                                                                      </div>
+                                                                 </div>
+                                   </div>
 
 
 
@@ -97,122 +96,122 @@ const Landing = () => (
          
 
 
-        </div>
+          </div>
 
-        <div className="flex w-full pt-[10%] pb-[5%] ">
+          <div className="flex w-full pt-[10%] pb-[5%] ">
 
 
                
 
                       <div className=" flex flex-col w-full h-full ml-[12%] ">
-                              <div className="flex rounded-2xl w-[50%] h-[7rem] shadow-xl justify-center items-center space-x-[10%]">
+                                   <div className="flex rounded-2xl w-[50%] h-[7rem] shadow-xl justify-center items-center space-x-[10%]">
  
-                                      <div>
-                                           <img src="/images/icon2.png" />
-                                      </div>
-
-                                      <div className="">
-                                              <div className="font-Montserrat   font-semibold text-3xl  ">
-                                                   Website Design
+                                             <div>
+                                                       <img src="/images/icon2.png" />
                                              </div>
-                                             <div className="  text-right">
-                                                 0 Projects
 
-                                              </div>
-                                    </div>
-                             </div>
+                                             <div className="">
+                                                       <div className="font-Montserrat   font-semibold text-3xl  ">
+                                                                      Website Design
+                                                       </div>
+                                                       <div className="  text-right">
+                                                                      0 Projects
 
-                             <div className="pt-[10%]">
+                                                       </div>
+                                             </div>
+                                   </div>
+
+                                   <div className="pt-[10%]">
           
-                             <div className="flex  rounded-2xl w-[50%]  h-[7rem] shadow-xl justify-center items-center space-x-[2%]">
+                                             <div className="flex  rounded-2xl w-[50%]  h-[7rem] shadow-xl justify-center items-center space-x-[2%]">
 
-                                      <div>
-                                           <img src="/images/icon3.png" />
-                                      </div>
+                                                       <div>
+                                                                      <img src="/images/icon3.png" />
+                                                       </div>
 
-                                      <div className="">
-                                              <div className="font-Montserrat font-semibold text-3xl  ">
-                                                   Mobile App Design
+                                                       <div className="">
+                                                                      <div className="font-Montserrat font-semibold text-3xl  ">
+                                                                                          Mobile App Design
+                                                                      </div>
+                                                                      <div className="text-right">
+                                                                                          0 Projects
+
+                                                                      </div>   
+                                                       </div>
                                              </div>
-                                             <div className="text-right">
-                                                 0 Projects
 
-                                              </div>
-                                    </div>
-                             </div>
+                                   </div>
 
-                             </div>
-
-                             <div className="pt-[10%]">
+                                   <div className="pt-[10%]">
           
-                             <div className="flex  rounded-2xl w-[50%]  h-[7rem] shadow-xl justify-center items-center space-x-[30%]">
+                                             <div className="flex  rounded-2xl w-[50%]  h-[7rem] shadow-xl justify-center items-center space-x-[30%]">
 
-                                      <div className="">
-                                           <img src="/images/icon4.png" />
-                                      </div>
+                                                       <div className="">
+                                                                 <img src="/images/icon4.png" />
+                                                       </div>
 
-                                      <div className="">
-                                              <div className="font-Montserrat font-semibold text-3xl  ">
-                                                   Feedbacks
-                                             </div>
+                                                       <div className="">
+                                                                 <div className="font-Montserrat font-semibold text-3xl  ">
+                                                                                     Feedbacks
+                                                                 </div>
                                              
-                                    </div>
-                             </div>
+                                                       </div>
+                                             </div>
 
-                             </div>
+                                   </div>
           
             
-                     </div>
+                         </div>
 
 
-        <div className="flex-col  w-full">
+                         <div className="flex-col  w-full">
 
-             <div className="font-Montserrat font-bold text-7xl">
-                  What Do I Help?
-             </div>
+                                   <div className="font-Montserrat font-bold text-7xl">
+                                                  What Do I Help?
+                                   </div>
 
-             <div className="font-Montserrat font-medium text-3xl pt-[10%] w-[92%]">
-                             <div> 
-                                  I help you with finding a solution and solve your problems. I use Next.js, Tailwind CSS and Typescript
-                                   to make WebApp.
-                            </div>     
+                                   <div className="font-Montserrat font-medium text-3xl pt-[10%] w-[92%]">
+                                             <div> 
+                                                       I help you with finding a solution and solve your problems. I use Next.js, Tailwind CSS and Typescript
+                                                       to make WebApp.
+                                             </div>     
 
-                            <div className="pt-[5%]">
-                                I give my best to fulfill your requirements and make website more user-friendly.
-                            </div>
+                                             <div className="pt-[5%]">
+                                                       I give my best to fulfill your requirements and make website more user-friendly.
+                                             </div>
 
-             </div>
+                                   </div>
 
-             <div className="flex space-x-[30%] justify-center pt-[4.5%]">
-                           <div className="flex flex-col">
-                                 <div className="text-7xl text-center" >
-                                      0+
-                                 </div>
+                               <div className="flex space-x-[30%] justify-center pt-[4.5%]">
+                                        <div className="flex flex-col">
+                                                   <div className="text-7xl text-center" >
+                                                                 0+
+                                                   </div>
 
-                                 <div className="font-Montserrat">
-                                      Projects Completed
-                                 </div>
+                                                  <div className="font-Montserrat">
+                                                       Projects Completed
+                                                  </div>
 
-                           </div>
+                                        </div>
 
-                           <div className="flex flex-col">
-                                 <div className="text-7xl text-center" >
-                                      0+
-                                 </div>
+                                        <div className="flex flex-col">
+                                                  <div className="text-7xl text-center" >
+                                                                  0+
+                                                  </div>
 
-                                 <div className="font-Montserrat">
-                                      Happy Clients
-                                 </div>
+                                                  <div className="font-Montserrat">
+                                                             Happy Clients
+                                                  </div>
 
-                           </div>
-
-
-             </div>
-
-        </div> 
+                                         </div>
 
 
-        </div>
+                              </div>
+
+                         </div> 
+
+
+                    </div>
     </div>
 
 )
